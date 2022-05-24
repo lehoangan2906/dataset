6610701567c8029a0ce6d76772d3140ac0ea1e7c
Le Hoang An <75709930+lehoangan2906@users.noreply.github.com>
2022-05-24 10:31:45 +0700

    Add files via upload

--- splitfolder/splitdata.py
+++ splitfolder/splitdata.py
@@ -1,31 +1,51 @@
-import os.path
+import os
 from os import path
 import shutil
 import glob
+from typing import List
+import pathlib
 
 root_folder = "/home/saplap/Giangnh/Unlabel"
-copy_folder = ["home/saplap/Giangnh/tolabel/Giangcoi", "home/saplap/Giangnh/tolabel/An", "home/saplap/Giangnh/tolabel/Giangbeo", "home/saplap/Giangnh/tolabel/Datden"
-        , "home/saplap/Giangnh/tolabel/Datvang" ,"home/saplap/Giangnh/tolabel/DuyTung", "home/saplap/Giangnh/tolabel/Hieu", "home/saplap/Giangnh/tolabel/HuyHoang", "home/saplap/Giangnh/tolabel/HoangSon"
-        , "home/saplap/Giangnh/tolabel/GiaBinh", "home/saplap/Giangnh/tolabel/TheHung", "home/saplap/Giangnh/tolabel/KienHoang", "home/saplap/Giangnh/tolabel/ThanhTra", "home/saplap/Giangnh/tolabel/KhanhDuong",
-        "home/saplap/Giangnh/tolabel/DoKhai", "home/saplap/Giangnh/tolabel/huytuan"]
+copy_folder = [" ", "/home/saplap/Giangnh/tolabel/Giangcoi",
+               "/home/saplap/Giangnh/tolabel/An",
+               "/home/saplap/Giangnh/tolabel/Giangbeo",
+               "/home/saplap/Giangnh/tolabel/Datden",
+               "/home/saplap/Giangnh/tolabel/Datvang",
+               "/home/saplap/Giangnh/tolabel/DuyTung",
+               "/home/saplap/Giangnh/tolabel/Hieu",
+               "/home/saplap/Giangnh/tolabel/HuyHoang",
+               "/home/saplap/Giangnh/tolabel/GiaBinh",
+               "/home/saplap/Giangnh/tolabel/TheHung",
+               "/home/saplap/Giangnh/tolabel/KienHoang",
+               "/home/saplap/Giangnh/tolabel/ThanhTra",
+               "/home/saplap/Giangnh/tolabel/KhanhDuong",
+               "/home/saplap/Giangnh/tolabel/DoKhai",
+               "/home/saplap/Giangnh/tolabel/HuyTuan"]
 
 # split into equal amount of images for each person
-m = len(root_folder)
+
 n = len(copy_folder) - 1
+m = len(os.listdir(root_folder))
 alpha = m/n
-i = 0
+print(alpha)
+
+i = 1   # i represents the index of each person in the copy_folder
 # traverse through every image files in root_folder
-for indx, label in enumerate(root_folder):
-     # i represents the index of each person in the copy_folder
-    
+for indx, label in enumerate(os.listdir(root_folder)):
+
+    label_path = os.path.join(root_folder, label)
     # if index of the image not exceed the amount of images given to the user
+    print(i)
+    if (indx >= int(i*alpha)):
+        i += 1
     if (indx < i*alpha):
-        
-        # if the folder storing data for the initital user exist 
-        if (path.exists(copy_folder[i])):
+        # if the folder storing data for the initital user exist
+        if (os.path.exists(copy_folder[i])):
             try:
-                # copy the image to the folder
-                shutil.copyfile(label, copy_folder[i])
+                new_file = copy_folder[i] + "/" + label
+                f = pathlib.Path(new_file)
+                f.touch()
+                shutil.copyfile(label_path, new_file)
             except shutil.SameFileError:
                 print("Source and destination represents the same file.")
             except IsADirectoryError:
@@ -34,7 +54,9 @@ for indx, label in enumerate(root_folder):
                 print("Permission denied.")
             except:
                 print("Error occured while copying file.")
-    else:
-        # index of the image exceed the limit of the initial user,
-        # proceed to the next user
-        i += 1
+        else:
+            os.mkdir(copy_folder[i])
+            newfile = copy_folder[i] + "/" + label
+            f = pathlib.Path(new_file)
+            f.touch()
+            shutil.copyfile(label_path, new_file)
